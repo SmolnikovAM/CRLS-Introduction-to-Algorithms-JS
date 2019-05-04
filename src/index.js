a4c4363fb6275022aa5ed28cbeb37ba6d2f71b9f
Andrei <30951971+SmolnikovAM@users.noreply.github.com>
2019-05-04 19:02:02 +0300

    mergeSort without flags

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-require('./part-01/mergeSort').run();
+(function currentTask() {
+  require('./part-01/mergeSortWithoutFlags').run();
+})();
+
+function allAlgorithms() {
+  require('./part-01/mergeSortWithoutFlags').run();
+  require('./part-01/mergeSort').run();
+}
